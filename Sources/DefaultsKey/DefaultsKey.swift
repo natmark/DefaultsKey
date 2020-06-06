685817d09e43558d57ec100f693ca119fdc64707
natmark <natmark0918@gmail.com>
2020-06-06 17:26:41 +0900

    Add DefaultsKey and extension

--- Sources/DefaultsKey/DefaultsKey.swift
+++ Sources/DefaultsKey/DefaultsKey.swift
@@ -1,3 +1,47 @@
-struct DefaultsKey {
-    var text = "Hello, World!"
+import Foundation
+
+fileprivate protocol OptionalProtocol {
+    var isNil: Bool { get }
+}
+
+extension Optional: OptionalProtocol {
+    fileprivate var isNil: Bool {
+        return self == nil
+    }
+}
+
+public struct DefaultsKey<T> {
+    public var key: String
+    public var defaultValue: T
+
+    public init(key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+}
+
+public extension UserDefaults {
+    func set<T: Codable>(value: T, forKey: DefaultsKey<T>) {
+        if let value = value as? OptionalProtocol, value.isNil {
+            removeObject(forKey: forKey.key)
+        } else {
+            let encoder = JSONEncoder()
+            if let json = try? encoder.encode(value) {
+                set(json, forKey: forKey.key)
+            }
+        }
+    }
+
+    func value<T: Codable>(forKey: DefaultsKey<T>) -> T {
+        if !dictionaryRepresentation().keys.contains(forKey.key) {
+            return forKey.defaultValue
+        } else {
+            let decoder = JSONDecoder()
+
+            guard let data = data(forKey: forKey.key), let object = try? decoder.decode(T.self, from: data) else {
+                return forKey.defaultValue
+            }
+            return object
+        }
+    }
 }
